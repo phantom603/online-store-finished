3ec5c153f4c5f68b0cb3c67f5884e034303bfad5
Volodymyr Shevchuk <dosandkv@gmail.com>
2024-02-23 01:19:32 +0200

    add navBar component

--- src/app.js
+++ src/app.js
@@ -1,112 +1,44 @@
-// NOTE: this import needed for bootstrap navigation toaster icon
-import { Offcanvas } from "bootstrap";
-
-import Modal from "./components/modal/index.js";
-import LoginForm from "./components/login-form/index.js";
-
-import { signOut } from "./api/auth.js";
+import NavigationBar from "./components/navigation-bar/index.js";
 
 import "./app.css";
 
 export default class App {
   element;
+  components = {};
 
   constructor() {
+    this.components.navigationBar = new NavigationBar();
+
     this.render();
     this.getSubElements();
+    this.renderComponents();
     this.initEventListeners();
   }
 
-  initEventListeners() {
-    const { signinBtn, signoutBtn } = this.subElements;
+  renderComponents() {
+    Object.keys(this.components).forEach((component) => {
+      const root = this.subElements[component];
+      const { element } = this.components[component];
 
-    signinBtn.addEventListener("click", () => {
-      const modal = new Modal();
-
-      const loginForm = new LoginForm(() => {
-        modal.close();
-      });
-
-      modal.addComponent(loginForm);
-      modal.open();
-    });
-
-    signoutBtn.addEventListener("click", async () => {
-      try {
-        await signOut();
-
-        this.element.dispatchEvent(
-          new CustomEvent("show-success-alert", {
-            bubbles: true,
-            detail: "Logout success",
-          }),
-        );
-        this.element.dispatchEvent(
-          new CustomEvent("logout", {
-            bubbles: true,
-          }),
-        );
-      } catch (error) {
-        this.element.dispatchEvent(
-          new CustomEvent("show-error-alert", {
-            bubbles: true,
-            detail: error.message,
-          }),
-        );
+      if (element) {
+        root.append(element);
       }
+    });
+  }
+
+  initEventListeners() {
+    document.addEventListener("login", () => {
+      this.components.navigationBar.update();
+    });
+    document.addEventListener("logout", () => {
+      this.components.navigationBar.update();
     });
   }
 
   get template() {
     return `
       <main class="app-main">
-        <header class="header">
-
-          <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid p-0">
-
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav justify-content-end flex-grow-1">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/home">Home</a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/create-product">Create product</a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/payment">Payment</a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/payment-status">Payment Status</a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/login">Login page</a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/orders">Orders</a>
-                  </li>
-
-                  <li class="nav-item">
-                    <button type="button" class="btn btn-link" data-element="signinBtn">Login</button>
-                  </li>
-                  <li class="nav-item">
-                    <button type="button" class="btn btn-link" data-element="signoutBtn">Logout</button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-
-        </header>
+        <header class="header" data-element="navigationBar"></header>
         <div id="content" class="content"></div>
       </main>
     `;
